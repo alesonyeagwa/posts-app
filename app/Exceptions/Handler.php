5043fb2fa89ef99df3797deeba88babf53867da5
alesonyeagwa <alesonyeagwa@gmail.com>
2021-04-26 12:32:57 +0100

    JSON API setup/config

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,12 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Neomerx\JsonApi\Exceptions\JsonApiException;
+use CloudCreativity\LaravelJsonApi\Exceptions\HandlesErrors;
 
 class Handler extends ExceptionHandler
 {
+    use HandlesErrors;
     /**
      * A list of the exception types that are not reported.
      *
@@ -14,6 +17,7 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         //
+        JsonApiException::class,
     ];
 
     /**
@@ -27,6 +31,15 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    public function render($request, Throwable $e)
+    {
+        //if ($this->isJsonApi($request, $e)) {
+        return $this->renderJsonApi($request, $e);
+        //}
+
+        //return parent::render($request, $e);
+    }
+
     /**
      * Register the exception handling callbacks for the application.
      *
@@ -37,5 +50,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function prepareException(Throwable $e)
+    {
+        if ($e instanceof JsonApiException) {
+            return $this->prepareJsonApiException($e);
+        }
+
+        return parent::prepareException($e);
     }
 }
